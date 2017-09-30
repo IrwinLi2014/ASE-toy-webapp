f0f0a1c3585a7df8971d22e30f161f65dc050e18
Wanlin Xie <wanlinx@gmail.com>
2017-09-30 13:55:27 -0400

    added mongodb, alert doesnt display

--- run.py
+++ run.py
@@ -1,8 +1,13 @@
 
-# import pymongo
-from flask import Flask, flash, redirect, render_template, request, session, abort
+
+from flask import Flask, flash, redirect, render_template, request, session, abort, jsonify
+from flask_pymongo import PyMongo
 app = Flask(__name__)
 
+app.config['MONGO_DBNAME']='toydb'
+app.config['MONGO_URI'] = 'mongodb://localhost:27017/toydb'
+
+mongo = PyMongo(app)
 """
 1.  Programming languages: Python, HTML, JavaScript
 2.  Web Development Application Framework: Flask
@@ -20,8 +25,16 @@ def index():
 
 @app.route("/add_stock", methods=["POST"])
 def add_stock():
-    print(request.form)
-    return ""
+    print(request.form["ticker"])
+    stocks = mongo.db.stocks # create a collection called stocks
+    ticker = request.form["ticker"]
+    ticker_id = stocks.insert({"ticker": ticker})
+
+    output=[]
+    for s in stocks.find():
+        output.append({'ticker':s['ticker']})
+    
+    return jsonify({'result': output})
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000)
--- static/js/script.js
+++ static/js/script.js
@@ -1,6 +1,8 @@
 $(document).ready(function() {
 	$("#add").click(function() {
 		var ticker = $('input[name="st"]').val();
-		$.post("add_stock", {"ticker": ticker});
+		$.post("add_stock", {"ticker": ticker}, function(data) {
+			alert(data);
+		});
 	});
 });
